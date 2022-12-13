0ba0df3f93c198ef650b6dfd43b1d8c5ee596ede
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2022-12-13 23:15:58 +0000

    Add code to (hopefully) solve part two

--- 2022/day13.py
+++ 2022/day13.py
@@ -24,16 +24,32 @@ def compare(a,b):
     else:
       return CONTINUE
     
+def solve_part_one():
+  result = []
+  i = 0
+  while i < len(input):
+    first = input[i]
+    second = input[i+1]
+    i += 2
+    pair_num = i/2
+    if compare(first,second) != WRONG_ORDER:
+      result.append(pair_num) 
 
-result = []
-i = 0
-while i < len(input):
-  first = input[i]
-  second = input[i+1]
-  i += 2
-  pair_num = i/2
-  if compare(first,second) != WRONG_ORDER:
-    result.append(pair_num) 
+  print(result)
+  print(sum(result))
 
-print(result)
-print(sum(result))
+solve_part_one()
+
+def solve_part_two():
+  first_divider_packet = [[2]]
+  second_divider_packet = [[6]]
+  position_of_first_divider = 1
+  position_of_second_divider = 1
+  for p in input:
+    if compare(p,first_divider_packet) == RIGHT_ORDER:
+      position_of_first_divider += 1
+    if compare(p,second_divider_packet) == RIGHT_ORDER:
+      position_of_second_divider += 1
+  print(position_of_first_divider * position_of_second_divider)
+
+solve_part_two()
