d716f6a40aae13e3526bdde7a2786b8b1da153aa
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2022-12-13 21:48:30 +0000

    Try to generate valid Python source code

--- 2022/Convert-InputToPython.ps1
+++ 2022/Convert-InputToPython.ps1
@@ -1,6 +1,7 @@
 $outputFile = "input.py"
-"input = [" | Out-File $outputFile
+"input = [" | Out-File $outputFile -Encoding utf8
 Get-Content "Input-Day13.txt" | Where-Object { $_ } | Foreach-Object {
-    "$_," | Out-File $outputFile -Append
+    "$_," | Out-File $outputFile -Append -Encoding utf8
 }
-"]" | Out-File $outputFile -Append
+"]" | Out-File $outputFile -Append -Encoding utf8
+"" | Out-File $outputFile -Append -Encoding utf8
