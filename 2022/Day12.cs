78ec4e78539de39b61f9470870b9b181a2fb117f
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2022-12-14 12:44:31 +0000

    Code to solve days 12 and 14

---
+++ 2022/Day12.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace DavidDylan.AdventOfCode2022
+{
+   public static class DayTwelve
+   {
+      const string InputFilePath = "Input-Day12.txt";
+
+      [Test]
+      public static void SolvePartOne()
+      {
+         var grid = LoadGrid();
+         Console.WriteLine(Solve(grid, Enumerable.Repeat(grid.Start,1)));
+      }
+
+      [Test]
+      public static void SolvePartTwo()
+      {
+         var grid = LoadGrid();
+         Console.WriteLine(Solve(grid, grid.GetLowestCells()));
+      }
+
+      public static int Solve(ForestGrid grid, IEnumerable<GridCell> startCells)
+      {
+         //Console.WriteLine($"Start at {grid.Start}");
+         //Console.WriteLine(string.Join(',', grid.GetReachableCells(grid.Start).Select(c => c.ToString())));
+         Dictionary<Point,int> distances = new Dictionary<Point, int>();
+         int distance = 0;
+         Queue<GridCell> nextFrontier = new Queue<GridCell>();
+         foreach (var startCell in startCells)
+         {
+            distances.Add(startCell.Position, distance);
+            nextFrontier.Enqueue(startCell);
+         }
+         while (!distances.ContainsKey(grid.End.Position))
+         {
+            distance++;
+            var currentFrontier = nextFrontier;
+            nextFrontier = new Queue<GridCell>();
+
+            while (currentFrontier.Any())
+            {
+               var frontierCell = currentFrontier.Dequeue();
+               foreach (var reachableCell in grid.GetReachableCells(frontierCell))
+               {
+                  if (!distances.ContainsKey(reachableCell.Position))
+                  {
+                     nextFrontier.Enqueue(reachableCell);
+                     distances.Add(reachableCell.Position, distance);
+                  }
+               }
+            }
+         }
+         return distances[grid.End.Position];
+      }
+
+      public static ForestGrid LoadGrid()
+      {
+         string[] lines = File.ReadAllLines(InputFilePath);
+         GridCell start = null;
+         GridCell end = null;
+         GridCell[,] grid = new GridCell[lines[0].Length,lines.Length];
+         for (int r = 0; r < lines.Length; r++)
+         {
+            for (int c = 0; c < lines[0].Length; c++)
+            {
+               char height = lines[r][c];
+               if (height == 'S')
+               {
+                  start = new GridCell(c, r, 'a');
+                  grid[c,r] = start;
+               }
+               else if (height == 'E')
+               {
+                  end = new GridCell(c,r,'z');
+                  grid[c,r] = end;
+               }
+               else
+               {
+                  grid[c,r] = new GridCell(c,r,height);
+               }
+            }
+         }
+         return new ForestGrid(grid, start, end, lines[0].Length, lines.Length);
+      }
+   }
+
+   public class Grid<T>
+   {
+      public T[,] Cells { get; private set; }
+      public int Height { get; private set; }
+      public int Width { get; private set; }
+
+      public Grid(T[,] cells, int width, int height)
+      {
+         Cells = cells;
+         Width = width;
+         Height = height;
+      }
+   }
+
+   public class ForestGrid : Grid<GridCell>
+   {
+      public GridCell Start { get; private set; }
+      public GridCell End { get; private set; }
+
+      public ForestGrid(GridCell[,] cells, GridCell start, GridCell end, int width, int height)
+         : base(cells, width, height)
+      {
+         Start = start;
+         End = end;
+      }
+
+      public IEnumerable<GridCell> GetLowestCells()
+      {
+         for (int c = 0; c < Width; c++)
+         {
+            for (int r = 0; r < Height; r++)
+            {
+               if (Cells[c, r].Height == 'a')
+               {
+                  yield return Cells[c, r];
+               }
+            }
+         }
+      }
+
+      public IEnumerable<GridCell> GetReachableCells(GridCell fromCell)
+      {
+         if (fromCell.Position.X > 0)
+         {
+            var cellToLeft = Cells[fromCell.Position.X - 1, fromCell.Position.Y];
+            if (fromCell.CanGetTo(cellToLeft))
+            {
+               yield return cellToLeft;
+            }
+         }
+         if (fromCell.Position.X < Width - 1)
+         {
+            var cellToRight = Cells[fromCell.Position.X + 1, fromCell.Position.Y];
+            if (fromCell.CanGetTo(cellToRight))
+            {
+               yield return cellToRight;
+            }
+         }
+         if (fromCell.Position.Y > 0)
+         {
+            var cellAbove = Cells[fromCell.Position.X, fromCell.Position.Y - 1];
+            if (fromCell.CanGetTo(cellAbove))
+            {
+               yield return cellAbove;
+            }
+         }
+         if (fromCell.Position.Y < Height - 1)
+         {
+            var cellBelow = Cells[fromCell.Position.X, fromCell.Position.Y + 1];
+            if (fromCell.CanGetTo(cellBelow))
+            {
+               yield return cellBelow;
+            }
+         }
+      }
+   }
+
+   public class GridCell
+   {
+      public Point Position { get; private set; }
+      public char Height { get; private set; }
+
+      public GridCell(int column, int row, char height)
+      {
+         Position = new Point(column, row);
+         Height = height;
+      }
+
+      public bool CanGetTo(GridCell toCell)
+      {
+         return toCell.Height <= this.Height + 1;
+      }
+
+      public override string ToString()
+      {
+         return $"({Position.X},{Position.Y}): {Height}";
+      }
+   }
+}
---
+++ 2022/Day14.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace DavidDylan.AdventOfCode2022
+{
+   public static class DayFourteen
+   {
+      const string InputFilePath = "Input-Day14.txt";
+
+      [Test]
+      public static void SolveExample()
+      {
+         CaveGrid exampleGrid = LoadGrid("Example-Input-Day14.txt");
+         CaveGrid gridWithFloor = exampleGrid.WithFloor();
+         Solve(exampleGrid);
+         Solve(gridWithFloor);
+         Console.WriteLine(gridWithFloor);
+      }
+
+      [Test]
+      public static void SolvePartOne()
+      {
+         Solve(LoadGrid(InputFilePath));
+      }
+
+      [Test]
+      public static void SolvePartTwo()
+      {
+         Solve(LoadGrid(InputFilePath).WithFloor());
+      }
+
+      private static void Solve(CaveGrid grid)
+      {
+         Point sandStart = new Point(500 - grid.XOffset, 0 - grid.YOffset);
+         Point[] moves = new []
+         {
+            new Point(0, 1), // down one step
+            new Point(-1, 1), // one step down and to the left
+            new Point(1, 1), // one step down and to the right
+         };
+         var result = 0;
+         while (grid.DropSandUnit(sandStart, moves))
+            result++;
+         Console.WriteLine(result);
+      }
+
+      public static CaveGrid LoadGrid(string inputFilePath)
+      {
+         var rockPaths = File.ReadAllLines(inputFilePath).Select(line =>
+            line.Split(" -> ").Select(p => {
+               var parts = p.Split(",");
+               return new Point(int.Parse(parts[0]), int.Parse(parts[1]));
+            }).ToArray()
+         ).ToArray();
+
+         var allPoints = rockPaths.SelectMany(p => p);
+         var allX = allPoints.Select(p => p.X);
+         var minX = allX.Min() - 1; // leave space either side
+         var maxX = allX.Max() + 1;
+         var allY = allPoints.Select(p => p.Y);
+         var minY = 0;
+         var maxY = allY.Max();
+         var width = maxX - minX + 1;
+         var height = maxY - minY + 1;
+
+         CaveCell[,] cells = new CaveCell[width,height];
+         for (int c = 0; c < width; c++)
+            for (int r = 0; r < height; r++)
+               cells[c,r] = new CaveCell(c,r);
+
+         foreach (var rockPath in rockPaths)
+         {
+            for (var segmentEnd = 1; segmentEnd < rockPath.Length; segmentEnd++)
+            {
+               var segmentStart = segmentEnd - 1;
+               var delta = new Point(
+                  Math.Sign(rockPath[segmentEnd].X - rockPath[segmentStart].X),
+                  Math.Sign(rockPath[segmentEnd].Y - rockPath[segmentStart].Y)
+               );
+               for (var p = rockPath[segmentStart]; p != rockPath[segmentEnd]; p.Offset(delta))
+               {
+                  cells[p.X - minX, p.Y - minY].Content = CellContent.Rock;
+               }
+            }
+            var end = rockPath.Last();
+            cells[end.X - minX, end.Y - minY].Content = CellContent.Rock;
+         }
+
+         return new CaveGrid(cells, width, height, minX, minY);
+      }
+   }
+
+   public class CaveGrid : Grid<CaveCell>
+   {
+      public int XOffset { get; private set; }
+      public int YOffset { get; private set; }
+
+      public CaveGrid(CaveCell[,] cells, int width, int height, int xOffset, int yOffset)
+         : base(cells, width, height)
+      {
+         XOffset = xOffset;
+         YOffset = yOffset;
+      }
+
+      public bool DropSandUnit(Point start, Point[] moves)
+      {
+         if (Cells[start.X, start.Y].Content != CellContent.Air)
+            return false;
+         var currentPosition = start;
+         bool moved;
+         do
+         {
+            moved = false;
+            foreach (var move in moves)
+            {
+               var nextPosition = currentPosition;
+               nextPosition.Offset(move);
+               if (nextPosition.Y >= Height)
+               {
+                  return false; // dropped into the abyss
+               }
+               if (Cells[nextPosition.X, nextPosition.Y].Content == CellContent.Air)
+               { 
+                  moved = true;
+                  currentPosition = nextPosition;
+                  break;
+               }
+            }
+         } while (moved);
+         Cells[currentPosition.X, currentPosition.Y].Content = CellContent.Sand;
+         return true;
+      }
+
+      public CaveGrid WithFloor()
+      {
+         var newHeight = Height + 2;
+         var newWidth = Width + Height * 2; // guessing this will be enough?
+         CaveCell[,] gridWithFloor = new CaveCell[newWidth, newHeight];
+         for (int c = 0; c < newWidth; c++)
+         {
+            for (int r = 0; r < newHeight; r++)
+            {
+               gridWithFloor[c,r] = new CaveCell(c, r);
+            }
+            gridWithFloor[c,newHeight-1].Content = CellContent.Rock; // add floor
+         }
+         for (int c = 0; c < Width; c++)
+         {
+            for (int r = 0; r < Height; r++)
+            {
+               gridWithFloor[c + Height, r].Content = Cells[c,r].Content;
+            }
+         }
+         return new CaveGrid(gridWithFloor, newWidth, newHeight, XOffset - Height, YOffset);
+      }
+
+      public override string ToString()
+      {
+         StringBuilder result = new StringBuilder();
+         for (var r = 0; r < Height; r++)
+         {
+            for (var c = 0; c < Width; c++)
+            { 
+               switch (Cells[c,r].Content)
+               {
+                  case CellContent.Air:
+                     result.Append('.');
+                     break;
+                  case CellContent.Rock:
+                     result.Append('#');
+                     break;
+                  case CellContent.Sand:
+                     result.Append('o');
+                     break;
+               }
+            }
+            result.AppendLine();
+         }
+         return result.ToString();
+      }
+   }
+
+   public enum CellContent
+   {
+      Air,
+      Rock,
+      Sand
+   }
+
+   public class CaveCell : GridCell
+   {
+      public CellContent Content { get; set; } = CellContent.Air;
+
+      private const char HeightNotRelevant = '-';
+      public CaveCell(int column, int row)
+         : base(column, row, HeightNotRelevant)
+      {
+      }
+   }
+}
