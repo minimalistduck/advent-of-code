e3aa8f9947a3371a3cfe3f9b9a07c14cb4cd333b
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2022-12-18 18:16:25 +0000

    More efficient and transparent solution for Day 17
    Also fixed the problem with the previous version, which was that I had a static RockType (now RockFactory). So it could start with the wrong shape of rock when solving second and subsequent problems in the same run.

--- 2022/Day17.cs
+++ 2022/Day17.cs
@@ -10,118 +10,170 @@ namespace DavidDylan.AdventOfCode2022
    public static class DaySeventeen
    {
       private const string ExampleInput = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
-      private const string InputFilePath = "Input-Day17.txt";
+      private const string InputFilePath = @"D:\David\Coding\AdventOfCode2022\Input-Day17.txt";
+      private const long PartTwoRockCount = 1000000000000L;
+
+      public static void ReproduceProblemStatement()
+      {
+         Console.WriteLine(RockFall.SimulateVerbose(ExampleInput, 11L));
+      }
 
       public static void SolveExamplePartOne()
       {
-         Console.WriteLine("Example: {0}", RockFall.Simulate(ExampleInput, 2022) + 1);
+         Console.WriteLine(RockFall.Simulate(ExampleInput, 2022L));
       }
       
       public static string RealInput { get { return File.ReadAllText(InputFilePath).Trim(); } }
       
       public static void SolvePartOne()
       {
-         // This gives the wrong answer based on my input.
-         // I know this because I generated and submitted the correct answer!
-         // But I can't get the code back to a state where it generates that answer now.
-         // On the example input, it matches the 3068 given in the problem statement.
-         Console.WriteLine("Part one: {0}", RockFall.Simulate(RealInput, 2022) + 1);
+         Console.WriteLine(RockFall.Simulate(RealInput, 2022L));
       }
       
-      // Used to demonstrate that this approach does not scale well enough
-      // to give an answer for part two today.
-      public static void MeasurePerformance(int rockCount)
+      public static void EstimateTimeToSolvePartTwo()
       {
          var timer = Stopwatch.StartNew();
-         RockFall.Simulate(RealInput, rockCount);
-         Console.WriteLine(timer.Elapsed);
+         RockFall.Simulate(RealInput, PartTwoRockCount / 1_000_000L);
+         Console.WriteLine("Estimate that solving part two will take {0} hours", timer.Elapsed.TotalSeconds * 1_000_000 / 60 / 60);
       }
    }
 
    public static class RockFall
    {
-      public static int Simulate(string jetPattern, int rockCount)
+      public static long Simulate(string jetPattern, long rockCount)
       {
+         var rockFactory = new RockFactory();
          var gasJetSource = new GasJet(jetPattern);
          var chamber = new Chamber();
-         var gravity = new Point(0,-1);
-         for (int r = 1; r <= rockCount; r++)
+         var movements = new Action<Chamber, Rock>[] {
+            (ch, rk) => ch.TryMoveLeft(rk),
+            (ch, rk) => throw new InvalidOperationException(),
+            (ch, rk) => ch.TryMoveRight(rk)
+         };
+         for (long r = 1L; r <= rockCount; r++)
          {
-            var rock = RockType.Next(chamber.NextEntryPoint);
-            //Console.WriteLine($"Start rock at {chamber.NextEntryPoint}");
+            var rock = rockFactory.Next(chamber.TopOfTower + 4L);
             do
             {
-               chamber.TryMoveAcross(rock, gasJetSource.NextMovement());
-            } while (chamber.TryMoveDown(rock, gravity));
-            chamber.StopRock(rock);
-            //Console.WriteLine(r + " " + chamber.ToString());
+               movements[gasJetSource.NextMovement()](chamber, rock);
+            } while (chamber.TryMoveDown(rock));
          }
          return chamber.TopOfTower;
       }
+      
+      public static long SimulateVerbose(string jetPattern, long rockCount)
+      {
+         var rockFactory = new RockFactory();
+         var gasJetSource = new GasJet(jetPattern);
+         var chamber = new Chamber();
+         var movements = new Action<Chamber, Rock>[] {
+            (ch, rk) => ch.TryMoveLeft(rk),
+            (ch, rk) => throw new InvalidOperationException(),
+            (ch, rk) => ch.TryMoveRight(rk)
+         };
+         for (long r = 1L; r <= rockCount; r++)
+         {
+            var rock = rockFactory.Next(chamber.TopOfTower + 4L);
+            do
+            {
+               Console.WriteLine(chamber.ToString(rock));
+               Console.WriteLine();
+               movements[gasJetSource.NextMovement()](chamber, rock);
+            } while (chamber.TryMoveDown(rock));
+         }
+         return chamber.TopOfTower;
+      }
+
    }
 
-   public class RockType
+   public class RockFactory
    {
-      public readonly Point[] FilledPoints;
-
-      private RockType(params int[] filledPointsXThenY)
+      public static readonly int TemplateCount;
+      private static uint[][] mRockTemplates;
+      private int mNextRockTemplate;
+      
+      static RockFactory()
       {
-         List<Point> filledPoints = new List<Point>();
-         for (int i = 0; i < filledPointsXThenY.Length; i += 2)
-         {
-            filledPoints.Add(new Point(filledPointsXThenY[i], filledPointsXThenY[i+1]));
-         }
-         FilledPoints = filledPoints.ToArray();
+         var rockTemplates = new List<uint[]>();
+         // Each template encodes the rock at its starting position over the chamber
+         rockTemplates.Add(new uint[] {
+            0b000111100u
+         });
+         rockTemplates.Add(new uint[] {
+            0b000010000u,
+            0b000111000u,
+            0b000010000u
+         });
+         rockTemplates.Add(new uint[] {
+            0b000001000u,
+            0b000001000u,
+            0b000111000u
+         }.Reverse().ToArray());  // written top-to-bottom but used bottom-to-top
+         rockTemplates.Add(new uint[] {
+            0b000100000u,
+            0b000100000u,
+            0b000100000u,
+            0b000100000u
+         });
+         rockTemplates.Add(new uint[] {
+            0b000110000u,
+            0b000110000u
+         });
+         mRockTemplates = rockTemplates.ToArray();
+         TemplateCount = mRockTemplates.Length;
       }
       
-      private static RockType[] mRockTypes = null;
-      private static int mNextRockType = 0;
-      
-      public static Rock Next(Point startingPosition)
+      public RockFactory()
       {
-         return new Rock(TakeRockType(), startingPosition);
+         mNextRockTemplate = 0;
       }
       
-      private static RockType TakeRockType()
+      public Rock Next(long startingHeight)
       {
-         if (mRockTypes == null)
-         {
-            mRockTypes = new [] {
-               new RockType(0,0, 1,0, 2,0, 3,0),
-               new RockType(1,0, 0,1, 1,1, 2,1, 1,2),
-               new RockType(0,0, 1,0, 2,0, 2,1, 2,2),
-               new RockType(0,0, 0,1, 0,2, 0,3),
-               new RockType(0,0, 1,0, 0,1, 1,1)
-            };
-         }
-         var result = mRockTypes[mNextRockType];
-         mNextRockType = (mNextRockType + 1) % mRockTypes.Length;
-         return result;
+         var template = mRockTemplates[mNextRockTemplate];
+         mNextRockTemplate = (mNextRockTemplate + 1) % TemplateCount;
+         var mutableCopy = template.ToArray();
+         return new Rock(mutableCopy, startingHeight);
       }
    }
 
    public class Rock
    {
-      public readonly RockType Type;
-      public Point Position;
+      // Conventions: Height is the height of the bottom of this rock
+      // Outline[i] is the rock's row at Height + i
+      public long Height;
+      public readonly uint[] Outline;
       
-      public Rock(RockType type, Point startingPosition)
+      public Rock(uint[] outline, long startingHeight)
       {
-         Type = type;
-         Position = startingPosition;
+         Outline = outline;
+         Height = startingHeight;
       }
       
-      public IEnumerable<Point> OccupiedPositions
+      public void MoveLeft()
       {
-         get
+         for (var i = 0; i < Outline.Length; i++)
          {
-            foreach (var offset in Type.FilledPoints)
-            {
-               var result = Position;
-               result.Offset(offset);
-               yield return result;
-            }
+            Outline[i] = Outline[i] << 1;
          }
+      }
+      
+      public void MoveRight()
+      {
+         for (var i = 0; i < Outline.Length; i++)
+         {
+            Outline[i] = Outline[i] >> 1;
+         }
+      }
+      
+      public void MoveDown()
+      {
+         Height -= 1L;
+      }
+      
+      public void MoveUp()
+      {
+         Height += 1L;
       }
    }
 
@@ -135,79 +187,161 @@ namespace DavidDylan.AdventOfCode2022
          mPattern = pattern;
       }
       
-      public Point NextMovement()
+      public int NextMovement()
       {
          char nextJet = mPattern[mNextJet];
          mNextJet = (mNextJet + 1) % mPattern.Length;
          // '<' is 60, '>' is 62, which is handy :-)
-         return new Point((int)nextJet - 61, 0);
+         return (int)(nextJet - '<');
       }
    }
 
    public class Chamber
    {
-      // Convention: Bottom, Left and Right are all *inside* the chamber
-      public readonly long Bottom = 0;
-      public readonly int Left = 0;
-      public readonly int Right = 6;
-      public int TopOfTower = -1;
-      public readonly HashSet<Point> StoppedRocks = new HashSet<Point>();
-      public Point NextEntryPoint => new Point(2,TopOfTower+4);
+      public const uint FullRow = 0x1FFu;
+      public const uint EmptyRow = 0x101u;
+
+      public long TopOfTower = 0L;
+      private List<uint> mLiveContent = new List<uint>();
+      private long mPurgedRowCount = 0L;
+      private const int MinPurgeBenefit = 100;
+      private const int EmptyRowsAtTop = 10;
 
       public Chamber()
       {
+         mLiveContent.Add(FullRow);
+         for (int h = 0; h < EmptyRowsAtTop; h++)
+            mLiveContent.Add(EmptyRow);   
       }
       
-      public void StopRock(Rock rock)
+      public bool TryMoveLeft(Rock rockToMove)
       {
-         StoppedRocks.UnionWith(rock.OccupiedPositions);
-         TopOfTower = StoppedRocks.Max(p => p.Y);
-         PurgeLowerPositions();
+         rockToMove.MoveLeft();
+         bool result = HasFeasiblePosition(rockToMove);
+         if (!result)
+            rockToMove.MoveRight();
+         return result;
       }
       
-      // A performance optimisation - get rid of rock positions that can no longer influence
-      // where a new rock lands.
-      private void PurgeLowerPositions()
+      public bool TryMoveRight(Rock rockToMove)
       {
-         int[] highestAtEachX = new int[Right - Left + 1];
-         foreach (var p in StoppedRocks)
+         rockToMove.MoveRight();
+         bool result = HasFeasiblePosition(rockToMove);
+         if (!result)
+            rockToMove.MoveLeft();
+         return result;
+      }
+      
+      public bool TryMoveDown(Rock rockToMove)
+      {
+         rockToMove.MoveDown();
+         bool result = HasFeasiblePosition(rockToMove);
+         if (!result)
          {
-            highestAtEachX[p.X] = Math.Max(p.Y, highestAtEachX[p.X]);
+            rockToMove.MoveUp();
+            Stop(rockToMove);
          }
-         var lowestHigh = highestAtEachX.Min();
-         StoppedRocks.RemoveWhere(p => p.Y < lowestHigh);
+         return result;
       }
       
-      public void TryMoveAcross(Rock rockToMove, Point offset)
+      private bool HasFeasiblePosition(Rock rock)
       {
-         TryMove(rockToMove, offset, newPositions => newPositions.All(p => Left <= p.X && p.X <= Right));
-      }
-      
-      private void TryMove(Rock rockToMove, Point offset, Func<IEnumerable<Point>,bool> isInBounds)
-      {
-         var newPositions = new HashSet<Point>(rockToMove.OccupiedPositions.Select(
-            p => { p.Offset(offset); return p; }));
-         if (isInBounds(newPositions) && !StoppedRocks.Overlaps(newPositions))
+         // Convention: when rock is at height h, this corresponds to element h of mLiveContent.
+         // In other words, height 0 is the rock that forms the base of the chamber
+         if (rock.Height - mPurgedRowCount > int.MaxValue)
+            throw new ArithmeticException("Tower is too high");
+         var chamberOffset = (int)(rock.Height - mPurgedRowCount);
+         for (var i = 0; i < rock.Outline.Length; i++)
          {
-            rockToMove.Position.Offset(offset);
-            //Console.WriteLine($"Moved rock to {rockToMove.Position}");
+            if ((rock.Outline[i] & mLiveContent[i + chamberOffset]) > 0)
+              return false;
          }
+         return true;
+      }
+      
+      private void Stop(Rock rock)
+      {
+         // Update the chamber with this rock in its current position
+         if (rock.Height - mPurgedRowCount > int.MaxValue)
+            throw new ArithmeticException("Tower is too high");
+         var chamberOffset = (int)(rock.Height - mPurgedRowCount);
+         for (var i = 0; i < rock.Outline.Length; i++)
+         {
+            mLiveContent[i + chamberOffset] = mLiveContent[i + chamberOffset] | rock.Outline[i];
+         }
+         
+         // Resize the chamber if necessary
+         TopOfTower = Math.Max(TopOfTower, rock.Height + rock.Outline.Length - 1);
+         if (TopOfTower + EmptyRowsAtTop - mPurgedRowCount - mLiveContent.Count > int.MaxValue)
+            throw new ArithmeticException("Tower is too high");
+         int extraHeightNeeded = (int)(TopOfTower + EmptyRowsAtTop - mPurgedRowCount - mLiveContent.Count);
+         for (int i = 0; i < extraHeightNeeded; i++)
+         {
+            mLiveContent.Add(EmptyRow);
+         }
+         
+         // Purge any of mLiveContent that is no longer reachable from above
+         var h = mLiveContent.Count - 1;
+         var blocked = EmptyRow;
+         while (blocked != FullRow && h >= 0)
+         {
+            blocked |= mLiveContent[h];
+            h--;
+         }
+         if (h > MinPurgeBenefit)
+         {
+            mPurgedRowCount += h;
+            mLiveContent = new List<uint>(mLiveContent.Skip(h));
+         }
+      }
+      
+      private string ToChamberString(uint line)
+      {
+         char[] result = new char[9];
+         result[0] = '|';
+         result[8] = '|';
+         for (int i = 1; i < 8; i++)
+         {
+            uint mask = (0x1u << i);
+            result[8 - i] = (mask & line) > 0 ? '#' : '.';
+         }
+         return new string(result);
+      }
+      
+      private string WithRock(string line, uint rockLine)
+      {
+         char[] result = line.ToCharArray();
+         for (int i = 1; i < 8; i++)
+         {
+            uint mask = (0x1u << i);
+            if ((mask & rockLine) > 0)
+            {
+               result[8 - i] = '@';         
+            }
+         }
+         return new string(result);
+      }
+      
+      private string WithTopOfTower(string line)
+      {
+         char[] result = line.ToCharArray();
+         result[0] = '>';
+         result[8] = '<';
+         return new string(result);
+      }
+         
+      public string ToString(Rock rock)
+      {
+         var result = mLiveContent.Select(ToChamberString).ToArray();
+         if (mPurgedRowCount == 0)
+            result[0] = "+-------+";
          else
-         {
-            //Console.WriteLine($"Couldn't move rock by {offset}");
-         }
-      }
-
-      public bool TryMoveDown(Rock rockToMove, Point offset)
-      {
-         var previousPosition = rockToMove.Position;
-         TryMove(rockToMove, offset, newPositions => newPositions.All(p => p.Y >= Bottom));
-         return rockToMove.Position != previousPosition;
-      }
-      
-      public override string ToString()
-      {
-         return $"Tower height {TopOfTower}";
+            result[0] = ":::::::::";
+         if (TopOfTower > 0)
+            result[TopOfTower-mPurgedRowCount] = WithTopOfTower(result[TopOfTower-mPurgedRowCount]);
+         for (int h = 0; h < rock.Outline.Length; h++)
+            result[h+rock.Height-mPurgedRowCount] = WithRock(result[h+rock.Height-mPurgedRowCount], rock.Outline[h]); 
+         return string.Join(Environment.NewLine, result.Reverse());
       }
    }
 }
