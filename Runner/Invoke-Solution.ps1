9b6b6e790d2c96b865def5b0e83e8ebabfaad581
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2024-12-20 09:52:21 +0000

    Update Invoke-Solution.ps1

--- Runner/Invoke-Solution.ps1
+++ Runner/Invoke-Solution.ps1
@@ -1,5 +1,5 @@
 $year = "2024"
-$day = "15";
+$day = "11";
 
 $ideCodingPath = Join-Path $PSScriptRoot "..\\$year"
 $sourceFile = Join-Path $ideCodingPath "Day$($day).cs"
@@ -7,4 +7,4 @@ Copy-Item $sourceFile $PSScriptRoot
 #$SourceFile = Join-Path $IdeCodingPath "Day$($Day)-Old.cs"
 #Copy-Item $SourceFile $PSScriptRoot
 
-dotnet run RunMe.csproj
+dotnet run RunMe.csproj -- $ideCodingPath
