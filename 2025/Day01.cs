47a1499cfa85dc2e2d4110daba9cd3e7fc117cdf
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2025-12-02 19:51:31 +0000

    Skeleton program

--- 2025/Day01.cs
+++ 2025/Day01.cs
@@ -1,1 +1,10 @@
+public static class DayOneProgram
+{
+  public static void Main(string[] args)
+  {
+     var lines = File.ReadAllLines(args[0]).ToArray();
 
+     // TODO: Fill in the code
+     //Console.WriteLine(partOne);
+  }
+}
