e476e84e0ebbccf351753870745fb28a781836b7
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2025-12-02 20:12:16 +0000

    Correction - add loop

--- 2025/Day01.cs
+++ 2025/Day01.cs
@@ -6,15 +6,17 @@ public static class DayOneProgram
     var partOne = 0;
     var pointingAt = 50;
 
-    var dirn = line[0] == 'R' ? 1 : -1;
-    var distance = int.Parse(line.Substring(1,line.Length-1));
-    var offset = dirn * distance;
-    while (pointingAt + offset < 0) // is this loop needed?
-      offset += 100;
-    pointingAt = (pointingAt + offset) % 100;
-    if (pointingAt == 0)
-      partOne++;
-
+    foreach (var line in lines)
+    {
+      var dirn = line[0] == 'R' ? 1 : -1;
+      var distance = int.Parse(line.Substring(1,line.Length-1));
+      var offset = dirn * distance;
+      while (pointingAt + offset < 0) // is this loop needed?
+        offset += 100;
+      pointingAt = (pointingAt + offset) % 100;
+      if (pointingAt == 0)
+        partOne++;
+    }
     Console.WriteLine(partOne);
   }
 }
