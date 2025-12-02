3580f40522582f4987f1592fbdc20e50f036933b
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2025-12-02 20:06:57 +0000

    First attempt at Day 1 Part 1

--- 2025/Day01.cs
+++ 2025/Day01.cs
@@ -2,9 +2,19 @@ public static class DayOneProgram
 {
   public static void Main(string[] args)
   {
-     var lines = File.ReadAllLines(args[0]).ToArray();
+    var lines = File.ReadAllLines(args[0]).ToArray();
+    var partOne = 0;
+    var pointingAt = 50;
 
-     // TODO: Fill in the code
-     //Console.WriteLine(partOne);
+    var dirn = line[0] == 'R' ? 1 : -1;
+    var distance = int.Parse(line.Substring(1,line.Length-1));
+    var offset = dirn * distance;
+    while (pointingAt + offset < 0) // is this loop needed?
+      offset += 100;
+    pointingAt = (pointingAt + offset) % 100;
+    if (pointingAt == 0)
+      partOne++;
+
+    Console.WriteLine(partOne);
   }
 }
