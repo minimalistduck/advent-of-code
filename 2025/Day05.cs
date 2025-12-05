94883e9a6244e2f2508dc2e84ce14e516787ebef
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2025-12-05 17:43:23 +0000

    First attempt at part 1

--- 2025/Day05.cs
+++ 2025/Day05.cs
@@ -9,9 +9,32 @@ public static class DayFiveProgram
 
   private static void SolvePartOne(string[] lines)
   {
-    var partOne = 0;
+    var rangeLines = new List<string>();
+    var ingredients = new List<Ingredient>();
+    var i = 0;
+    for (; !string.IsNullOrEmpty(lines[i]); i++)
+    {
+      rangeLines.Add(lines[i]);
+    }
+    i++;
+    for (; i < lines.Length; i++)
+    {
+      ingredients.Add(new Ingredient(long.Parse(lines[i])));
+    }
 
-    Console.WriteLine(partOne);
+    foreach (var rangeLine in rangeLines)
+    {
+      var rangeLineParts = rangeLine.Split("-");
+      var rangeEnds = rangeLineParts.Select(long.Parse).ToArray();
+
+      foreach (var ing in ingredients)
+      {
+        if (rangeEnds[0] <= ing.Id && ing.Id <= rangeEnds[1])
+          ing.IsFresh = true;
+      }
+    }
+
+    Console.WriteLine(ingredients.Count(ing => ing.IsFresh));
   }
 
   private static void SolvePartTwo(string[] lines)
@@ -20,4 +43,10 @@ public static class DayFiveProgram
 
     Console.WriteLine(partTwo);
   }
+}
+
+public class Ingredient(long _id)
+{
+  public long Id => _id;
+  public bool IsFresh { get; set; }
 }
