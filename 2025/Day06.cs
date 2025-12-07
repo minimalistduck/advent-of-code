47c8b736999f3843e5c457d7a7a9159b082cd5b5
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2025-12-07 14:22:29 +0000

    Use BigInteger type instead of long

--- 2025/Day06.cs
+++ 2025/Day06.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 public static class DaySixProgram
 {
   public static void Main(string[] args)
@@ -7,31 +9,40 @@ public static class DaySixProgram
     SolvePartOne(lines);
   }
 
+  private static BigInteger ParseBigInt(string item) =>
+    new BigInteger(long.Parse(item));
+
   private static void SolvePartOne(string[] lines)
   {
     var operations = lines.Last().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
     var results = lines.First()
       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-      .Select(long.Parse)
+      .Select(ParseBigInt)
       .ToArray();
-    var funcs = operations.Select<string,Func<long,long,long>>(op => op switch
+    var funcs = operations.Select<string,Func<BigInteger,BigInteger,BigInteger>>(op => op switch
       {
-        "+" => (long x,long y) => x+y,
-        "*" => (long x,long y) => x*y,
-        _ => (long x,long y) => x // unused
+        "+" => (BigInteger x, BigInteger y) => x+y,
+        "*" => (BigInteger x, BigInteger y) => x*y,
+        _ => (BigInteger x, BigInteger y) => x // unused
       }).ToArray();
     for (var ln = 1; ln < lines.Length - 2; ln++)
     {
       var operands = lines[ln].Split(" ", StringSplitOptions.RemoveEmptyEntries)
-        .Select(long.Parse).ToArray();
+        .Select(ParseBigInt).ToArray();
       for (var i = 0; i < operations.Length; i++)
       {
         results[i] = funcs[i](results[i], operands[i]);
       }
     }
+
+    var partTwo = new BigInteger(0);
+    foreach (var result in results)
+    {
+      partTwo = partTwo + result;
+    }
     
-    Console.WriteLine(results.Sum());
+    Console.WriteLine(partTwo);
   }
 
   private static void SolvePartTwo(string[] lines)
