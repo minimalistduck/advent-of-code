a280b84c2a360df8544234802f9d68a906eda6c4
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2025-12-07 14:29:31 +0000

    Note the problem and add some logging

--- 2025/Day06.cs
+++ 2025/Day06.cs
@@ -6,6 +6,7 @@ public static class DaySixProgram
   {
     var lines = File.ReadAllLines(args[1]).ToArray();
 
+    // 18510974039 is too low
     SolvePartOne(lines);
   }
 
@@ -14,18 +15,20 @@ public static class DaySixProgram
 
   private static void SolvePartOne(string[] lines)
   {
-    var operations = lines.Last().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-
     var results = lines.First()
       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
       .Select(ParseBigInt)
       .ToArray();
+    Console.WriteLine($"Initial value in first column is {results[0]}");
+    
+    var operations = lines.Last().Split(" ", StringSplitOptions.RemoveEmptyEntries);
     var funcs = operations.Select<string,Func<BigInteger,BigInteger,BigInteger>>(op => op switch
       {
         "+" => (BigInteger x, BigInteger y) => x+y,
         "*" => (BigInteger x, BigInteger y) => x*y,
         _ => (BigInteger x, BigInteger y) => x // unused
       }).ToArray();
+    
     for (var ln = 1; ln < lines.Length - 2; ln++)
     {
       var operands = lines[ln].Split(" ", StringSplitOptions.RemoveEmptyEntries)
@@ -33,6 +36,8 @@ public static class DaySixProgram
       for (var i = 0; i < operations.Length; i++)
       {
         results[i] = funcs[i](results[i], operands[i]);
+        if (i == 0)
+          Console.WriteLine($"{operations[i]} {operands[i]} => {results[i]}");
       }
     }
 
