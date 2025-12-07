bb077b8202a378c051e0744ce6efe910db7015ca
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2025-12-07 19:28:28 +0000

    First attempt at part 2

--- 2025/Day06.cs
+++ 2025/Day06.cs
@@ -6,8 +6,7 @@ public static class DaySixProgram
   {
     var lines = File.ReadAllLines(args[1]).ToArray();
 
-    // 18510974039 is too low
-    SolvePartOne(lines);
+    SolvePartTwo(lines);
   }
 
   private static BigInteger ParseBigInt(string item) =>
@@ -50,9 +49,62 @@ public static class DaySixProgram
     Console.WriteLine(partTwo);
   }
 
+  private HashSet<int> SpaceIndexes(string line)
+  {
+    var result = new HashSet<int>();
+    for (int c = 0; c < line.Length; c++)
+    {
+      if (line[c] == ' ')
+        result.Add(c);
+    }
+    result.Add(-1);
+    result.Add(line.Length);
+    return result;
+  }
+
   private static void SolvePartTwo(string[] lines)
   {
-    var partTwo = 0;
+    var partTwo = 0L;
+    
+    var spaceIndexSet = SpaceIndexes(lines[0]);
+    for (var ln = 1; ln < lines.Length - 1; ln++)
+    {
+      spaceIndexSet.IntersectWith(SpaceIndexes(lines[ln]));
+    }
+
+    var spaceIndexes = spaceIndexSet.ToArray();
+    Array.Sort(spaceIndexes);
+    Console.WriteLine("Columns where there's a space in all rows:");
+    Console.WriteLine(spaceIndexes.Join(","));
+
+    var operations = lines.Last().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    var funcs = operations.Select<string,Func<long,long,long>>(op => op switch
+      {
+        "+" => (long x, long y) => x+y,
+        "*" => (long x, long y) => x*y,
+        _ => (long x, long y) => x // unused
+      }).ToArray();
+
+    for (var prob = 0; prob < spaceIndexes.Length - 1; prob++)
+    {
+      var result = operations[prob] switch
+      {
+          "+" => 0L,
+          "*" => 1L,
+          _ => 0L // unused
+      };
+      for (var col = spaceIndexes[prob+1] - 1; col > spaceIndexes[prob]; col--)
+      {
+        var digits = new List<char>();
+        for (var row = 0; row < lines.Length - 1; row++)
+        {
+          digits.Add(lines[row][col]);
+        }
+        var operand = long.Parse(new string(digits.ToArray()).Trim());
+        result = funcs[prob](result, operand);
+      }
+      partTwo += result
+    }
 
     Console.WriteLine(partTwo);
   }
