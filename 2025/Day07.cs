9e6a7ef70ba9bd9880a94a9d222db654923f0b7e
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2025-12-09 21:18:39 +0000

    First attempt at part 2

--- 2025/Day07.cs
+++ 2025/Day07.cs
@@ -4,7 +4,7 @@ public static class DaySevenProgram
   {
     var lines = File.ReadAllLines(args[1]).ToArray();
 
-    SolvePartOne(lines);
+    SolvePartTwo(lines);
   }
 
   private static void SolvePartOne(string[] lines)
@@ -66,6 +66,55 @@ public static class DaySevenProgram
   private static void SolvePartTwo(string[] lines)
   {
     var partTwo = 0;
+
+    
+    var width = lines[0].Length + 2;
+    var height = lines.Length;
+    
+    var grid = new int[width,height];
+    
+    for (var x = 0; x < width; x++)
+    {
+      for (var y = 0; y < height; y++)
+      {
+        grid[x,y] = 0;
+      }
+    }
+    
+    for (var x = 1; x < width - 1; x++)
+    {
+      if (lines[0][x-1] == 'S')
+        grid[x,0] = 1;
+    }
+    
+    for (var y = 1; y < height; y++)
+    {
+      for (var x = 1; x < width - 1; x++)
+      {
+        if (lines[y][x-1] == '^')
+        {
+          grid[x,y] = -1;
+        }
+      }
+    }
+    
+    for (var row = 1; row < height; row++)
+    {
+      var rowAbove = row - 1;
+      for (var col = 1; col < width - 1; col++)
+      {
+        if (grid[col,row] == -1 && grid[col,rowAbove] > 0)
+        {
+          // assuming there isn't a splitter there too
+          grid[col-1,row] += grid[col, rowAbove];
+          grid[col+1,row] += grid[col, rowAbove];
+        }
+        else if (grid[col,rowAbove] > 0)
+        {
+          grid[col,row] = grid[col, rowAbove];
+        } 
+      }
+    }
 
     Console.WriteLine(partTwo);
   }
