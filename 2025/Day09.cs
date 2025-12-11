20bb2268a1eac0d7f5412c2a361c5a898d865dca
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2025-12-11 10:09:13 +0000

    First attempt at day 9

--- 2025/Day09.cs
+++ 2025/Day09.cs
@@ -9,9 +9,20 @@ public static class DayNineProgram
 
   private static void SolvePartOne(string[] lines)
   {
-    var partOne = 0;
+    var possibleCorners = lines.Select(Point.FromString).ToArray();
 
-    Console.WriteLine(partOne);
+    var pairs = new List<PairOfPoints>();
+    for (var i = 0; i < possibleCorners.Length - 1; i++)
+    {
+      for (var j = i+1; j < possibleCorners.Length; j++)
+      {
+        pairs.Add(new PairOfPoints(possibleCorners[i], possibleCorners[j]));
+      }
+    }
+
+    pairs.Sort((a,b) => a.AreaOfRectangle.CompareTo(b.AreaOfRectangle));
+
+    Console.WriteLine(pairs.Last().AreaOfRectangle);
   }
 
   private static void SolvePartTwo(string[] lines)
@@ -19,5 +30,39 @@ public static class DayNineProgram
     var partTwo = 0;
 
     Console.WriteLine(partTwo);
+  }
+}
+
+public class Point
+{
+  public readonly long X;
+  public readonly long Y;
+  
+  private Point(long x, long y)
+  {
+    X = x;
+    Y = y;
+  }
+  
+  public static Point FromString(string inputLine)
+  {
+    var split = inputLine.Split(",").Select(long.Parse).ToArray();
+    return new Point(split[0], split[1]);
+  }
+}
+
+public class PairOfPoints
+{
+  public readonly Point First;
+  public readonly Point Second;
+  public readonly long AreaOfRectangle; 
+
+  public PairOfPoints(Point first, Point second)
+  {
+    First = first;
+    Second = second;
+    var width = Math.Abs(second.X-first.X) + 1L;
+    var height = Math.Abs(second.Y-first.Y) + 1L;
+    AreaOfRectangle = width * height;
   }
 }
