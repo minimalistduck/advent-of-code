9bb83f8272e6424218d6bb5cf5ffceefe252e887
minimalistduck <41017067+minimalistduck@users.noreply.github.com>
2025-12-11 15:59:16 +0000

    Create Day10.cs

---
+++ 2025/Day10.cs
@@ -0,0 +1,23 @@
+public static class DayTenProgram
+{
+  public static void Main(string[] args)
+  {
+    var lines = File.ReadAllLines(args[1]).ToArray();
+
+    SolvePartOne(lines);
+  }
+
+  private static void SolvePartOne(string[] lines)
+  {
+    var partOne = 0;
+
+    Console.WriteLine(partOne);
+  }
+
+  private static void SolvePartTwo(string[] lines)
+  {
+    var partTwo = 0;
+
+    Console.WriteLine(partTwo);
+  }
+}
